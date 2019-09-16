dff54ffc7b4fe18c1cf1fa55d8686c1d2a66f54b
Thomas MONTERO <thomas99.montero@gmail.com>
2019-09-16 13:19:25 +0200

    added command line support

---
+++ main.py
@@ -0,0 +1,11 @@
+import parser
+import Pyscandl
+from fetchers import fetcher_enum
+
+if __name__ == "__main__":
+	args = parser.parse_arg()
+	fetcher = fetcher_enum.Fetcher.get(args.fetcher).value
+
+	pyscandl = Pyscandl.Pyscandl(fetcher, chapstart=args.chapter_start, output=args.output, keepimage=args.keep_images, all=args.all, link=args.link, manga=args.manga, download_number=args.download_number)
+
+	pyscandl.full_download()
