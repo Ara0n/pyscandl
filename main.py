2b64685c44c61af6d11e3850d30b5994a4f254fd
Thomas MONTERO <thomas99.montero@gmail.com>
2019-09-18 09:26:20 +0200

    included with the `fix module name to avoid conflict with builtin modules old commit`

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import parser
+import arg_parser
 import Pyscandl
 from fetchers import fetcher_enum
 
 if __name__ == "__main__":
-	args = parser.parse_arg()
+	args = arg_parser.parse_arg()
 	fetcher = fetcher_enum.Fetcher.get(args.fetcher).value
 
 	pyscandl = Pyscandl.Pyscandl(fetcher, chapstart=args.chapter_start, output=args.output, keepimage=args.keep_images, all=args.all, link=args.link, manga=args.manga, download_number=args.download_number, quiet=args.quiet, skip=args.start)
