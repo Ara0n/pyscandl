4692a8a3a9d21df97ce934dd7e9c73917307427a
Thomas MONTERO <thomas99.montero@gmail.com>
2019-11-09 01:49:48 +0100

    restructured options and subparsers

--- modules/arg_parser.py
+++ modules/arg_parser.py
@@ -4,17 +4,16 @@ from .fetchers.fetcher_enum import Fetcher
 
 def parse_arg():
 	parser = argparse.ArgumentParser()
-	subparsers = parser.add_subparsers()
+	subparsers = parser.add_subparsers(dest="subparser")
+	autodl = subparsers.add_parser("autodl", help="auto downloader using the mangas in the json")
+	manga = subparsers.add_parser("manga", help="tool to modify, add and remove mangas from the automatic rss downloader mode list")
 	manual_pars = subparsers.add_parser("manual", help="manually download scans, it will not update the downloaded scans json, if you plan on setting up a manga with the automatic rss mode don't mix both commands")
-	json = subparsers.add_parser("json", help="tool to modify, add and remove mangas from the automatic rss downloader mode list")
 
-	options = parser.add_mutually_exclusive_group()
-	options.add_argument("-U", "--update", action="store_true", help="updates the program")
-	options.add_argument("-l", "--list", action="store_true", help="list all the current mangas in the auto downloader")
-	options.add_argument("-m", "--manga", type=str, help="prints the info for the named manga in the auto updater")
-	options.add_argument("-d", "--delete", type=str, help="deletes the corresponding manga from the auto downloader")
+	parser.add_argument("-U", "--update", action="store_true", help="updates the program")
+	parser.add_argument("-ml", "--manga-list", action="store_true", help="list all the current mangas in the auto downloader")
 
-	# manual downloading parser
+
+	# manual downloading subparser
 	sauce = manual_pars.add_mutually_exclusive_group()
 	sauce.add_argument("-l", "--link", type=str, help="gives the link to the page with all the chapter listed")
 	sauce.add_argument("-m", "--manga", type=str, help="gives the manga name, the image fetcher will find the corresponding manga")
@@ -32,19 +31,26 @@ def parse_arg():
 	manual_pars.add_argument("-t", "--tiny", action="store_true", help="don't write the manga name in the title (useful if using ebook libraries)")
 
 
-	# json modification parser
-	json.add_argument("name", type=str, help="name for the stored manga")
-	edit_mode = json.add_mutually_exclusive_group()
-	edit_mode.add_argument("-a", "--add", action="store_true", help="add a new manga to the auto downloader")
-	edit_mode.add_argument("-e", "--edit", action="store_true", help="modify infos for one of the already existing manga in the auo downloader")
+	# json interaction subparser
+	manga.add_argument("name", type=str, help="name for the stored manga")
+	option = manga.add_mutually_exclusive_group(required=True)
+	option.add_argument("-a", "--add", action="store_true", help="add a new manga to the auto downloader")
+	option.add_argument("-e", "--edit", action="store_true", help="modify infos for one of the already existing manga in the auo downloader")
+	option.add_argument("-i", "--info", action="store_true", help="prints the info for the named manga in the auto updater")
+	option.add_argument("-d", "--delete", action="store_true", help="deletes the corresponding manga from the auto downloader")
 
-	json.add_argument("-r", "--rss", type=str, help="rss link with the update notification of the manga")
-	json.add_argument("-l", "--link", type=str, help="link of the manga")
-	json.add_argument("-f", "--fetcher", type=str, help="name of the fetcher needed for the manga")
-	json.add_argument("-c", "--chapters", nargs="*", help="list of all the chapters already downloaded to be added to the list for the autoupdater")
+	manga.add_argument("-r", "--rss", type=str, help="rss link with the update notification of the manga")
+	manga.add_argument("-l", "--link", type=str, help="link of the manga")
+	manga.add_argument("-f", "--fetcher", type=str, help="name of the fetcher needed for the manga")
+	manga.add_argument("-c", "--chapters", type=str, nargs="*", help="list of all the chapters already downloaded to be added to the list for the autoupdater")
 
-	parser.epilog = "The current list of image fetcher is: " + ", ".join(Fetcher.list())
-	manual_pars.epilog = parser.epilog
-	json.epilog = parser.epilog
+
+	# autodl subparser
+	autodl.add_argument("-o", "--output", type=str, default=".", help="the path (absolute or relative) to the folder where to save the data if using the -a option")
+	autodl.add_argument("-t", "--tiny", action="store_true", help="don't write the manga name in the title (useful if using ebook libraries) if using the -a option")
+	autodl.add_argument("-q", "--quiet", action="store_true", help="removes the verbose of the autodl")
+
+	manual_pars.epilog = "The current list of image fetcher is: " + ", ".join(Fetcher.list())
+	manga.epilog = manual_pars.epilog
 
 	return parser.parse_args()
