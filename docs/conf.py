d3f8593a8a8f0f56035dfa6be41521bd6c795421
_Ara0n_ <thomas99.montero@gmail.com>
2020-07-19 19:57:34 +0200

    fix masterdoc

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,9 @@ release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+# sets the good master doc
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
