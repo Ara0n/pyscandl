43ff6284335cc0aff4acb6643005071f2affbe97
_Ara0n_ <thomas99.montero@gmail.com>
2020-07-20 10:37:04 +0200

    forgot to specify the good master-doc name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,9 @@ release = '2.0.0'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+source_suffix = ".rst"
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
