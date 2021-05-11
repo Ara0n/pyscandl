307319ca39249f09d6dc6132c28cb04905b331ba
_ara0n_ <thomas99.montero@gmail.com>
2021-05-11 20:01:31 +0200

    chg: usr: changed the old mangadex fetcher for one using their new api, still in beta so might not be final

--- pyscandl/modules/fetchers/mangadex.py
+++ pyscandl/modules/fetchers/mangadex.py
@@ -1,213 +1,173 @@
-from ..excepts import MangaNotFound, EmptyChapter, DelayedRelease, DownedSite
-from .fetcher import Fetcher
-import cfscrape
 import requests
-import json.decoder
-from datetime import datetime
+
+from .fetcher import Fetcher
+from ..excepts import DownedSite, MangaNotFound
 
 
 class Mangadex(Fetcher):
-	__doc__ = """
-	This is the fetcher in charge of https://mangadex.org/ 
-	"""
+    __doc__ = """this is the new fetcher for mangadex using their new v5 api, the api is still in beta so all of this fetcher is subject to changes quickly"""
 
-	def __init__(self, link:str=None, manga:str=None, chapstart=1, lang=""):
-		"""
-		Initializes the instance of the nhentai fetcher, it needs either manga or link to work.
+    def __init__(self, link:str=None, manga:str=None, chapstart=1, lang=""):
+        """
+        Initializes the instance of the mangadex fetcher, it needs either manga or link to work.
 
-		:param link: link of the scan wanted
-		:type link: str
-		:param manga: numbered id corresponding to the manga on the website, ex: 286
-		:type manga: str
-		:param chapstart: number of the chapter that the download is supposed to start
-		:type chapstart: int/float/str
+        :param link: link of the wanted scan
+        :type link: str
+        :param manga: id of the wanted scan
+        :type manga: str
+        :param chapstart: chapter on which to start
+        :param lang: language used
+        :type lang: str
 
-		:raises MangaNotFound: the scan asked for can't be found
-		:raises EmptyChapter: the chapter you want to download is empty
-		:raises DelayedRelease: the chapter you want to download isn't available yet
-		"""
+        :raises MangaNotFound: couldn't find the manga that was asked for with the id or the link
+        """
+        super().__init__(link, manga, chapstart)
 
-		super().__init__(link, manga, chapstart)
+        # language stuff to fetch for specific locales
+        if lang:
+            self._lang = lang
+        self.domain = ".mangadex.org"
 
-		if lang:
-			self._lang = lang
-		self.domain = ".mangadex.org"
+        # init some api stuff that will be useful later
+        self._api_base = "https://api.mangadex.org"
+        self._api_manga_feed = self._api_base + "/manga/{uuid}/feed?limit=500&locales[]={locale}&order[chapter]=asc&offset={offset}"
 
-		self.scrapper = cfscrape.create_scraper()
+        # getting the legacy id from the links
+        # TODO: change that when the new links are out while giving a tool to do the change
+        if manga is not None:
+            legacy_id = int(manga)
+        elif link is not None:
+            if link[-1] == "/":
+                link = link[:-1]
+            legacy_id = int(link.split("/")[-2])
 
-		# getting the manga id
-		manga = str(manga)
-		if link is not None:
-			if link[-1] == "/":
-				link = link[:-1]
-			manga_id = link.split("/")[-2]
-		elif manga.isdecimal():
-			manga_id = manga
-		else:
-			# TODO: implement a search function for non id requests
-			raise MangaNotFound(manga)
+        # getting the new id from the legacy ones
+        req = requests.post(self._api_base + "/legacy/mapping", json={"type": "manga", "ids": [legacy_id]})
+        if req.status_code != 200:
+            raise MangaNotFound(f" legacy id {legacy_id}")
+        id = req.json()[0]["data"]["attributes"]["newId"]
 
+        # getting the manga title name (either english or romanji)
+        manga_req = requests.get(self._api_base + f"/manga/{id}")
+        if manga_req.status_code != 200:
+            raise MangaNotFound(f"id {id}")
+        manga = manga_req.json()
+        self.manga_name = list(manga["data"]["attributes"]["title"].values())[0]
 
-		try:
-			manga_json = self.scrapper.get(f"https://mangadex.org/api/manga/{manga_id}").json()
-		except json.decoder.JSONDecodeError:
-			raise DownedSite("Mangadex")
-		if manga_json.get("status") == "Manga ID does not exist.":
-			raise MangaNotFound(manga_id)
+        # getting the author name
+        author_id = next((relation["id"] for relation in manga["relationships"] if relation["type"]=="author"), None)
+        author_req = requests.get(self._api_base + f"/author/{author_id}")
+        if author_req.status_code == 200:
+            self.author = author_req.json()["data"]["attributes"]["name"]
 
-		self._chapters_json = manga_json.get("chapter")
+        # getting the chapter info from the feed
+        feed_req = requests.get(self._api_manga_feed.format(uuid=id, locale=self._lang, offset=0))
+        feed = feed_req.json()
+        self._chapinfo = [chap["data"] for chap in feed["results"]]
+        for i in range(500, int(feed["total"]), 500):
+            feed_req = requests.get(self._api_manga_feed.format(uuid=id, locale=self._lang, offset=i))
+            self._chapinfo.extend([chap["data"] for chap in feed_req.json()["results"]])
 
-		self._ordered_chaps_json = []
-		for item in sorted(self._chapters_json.keys(), key=lambda x: self._chapters_json[x]["chapter"].zfill(10)):
-			self._ordered_chaps_json.append((item, self._chapters_json.get(item)))
-		self._ordered_chaps_json = [chap for chap in self._ordered_chaps_json if chap[1].get("lang_code")==self._lang]
+        # checking if there are no duplicate numbers for chapters
+        unique_chaps = {chap["attributes"]["chapter"] for chap in self._chapinfo}
+        if len(self._chapinfo) != len(unique_chaps):
+            new_chap_list = []
+            for i in unique_chaps:
+                new_chap_list.append(next(chap for chap in self._chapinfo if chap["attributes"]["chapter"]==i))
+            self._chapinfo = sorted(new_chap_list, key=lambda x: float(x["attributes"]["chapter"]))
 
-		self.manga_name = manga_json.get("manga").get("title")
-		self.author = manga_json.get("manga").get("author")
+        self.go_to_chapter(next((chapter["attributes"]["chapter"] for chapter in self._chapinfo if chapter["attributes"]["chapter"] == str(chapstart))))
 
-		self.go_to_chapter(chapstart)
+    def go_to_chapter(self, chap):
+        """
+        Go to the specified chapter.
 
-	def _set_current_chap_info(self, chap_id):
-		"""
-		Method used to update the internal info of the parser about the chapter given with the chap_id.
+        :param chap: number of the specified chapter
 
-		:param chap_id: id of the chapter we want the info about
+        :raises MangaNotFound: the asked chapter doesn't exist
+        :raises DownedSite: the md@h service is down
+        """
 
-		:raises EmptyChapter: the chapter you want to download is empty
-		:raises DelayedRelease: the chapter you want to download isn't available yet
-		"""
+        try:
+            self._current_chap = next((chapter for chapter in self._chapinfo if chapter["attributes"]["chapter"] == str(chap)))
+        except StopIteration:
+            raise MangaNotFound(f"{self.manga_name}, chapter {chap}")
 
-		self.npage = 1
+        self.chapter_number = self._current_chap["attributes"]["chapter"]
+        self.chapter_name = self._current_chap["attributes"]["title"]
 
+        # get md@h corresponding server
+        md_at_h_server = requests.get(self._api_base + f"/at-home/server/{self._current_chap['id']}")
+        if md_at_h_server.status_code != 200:
+            raise DownedSite(self.domain)
+        self._server_url = md_at_h_server.json()["baseUrl"]
 
-		try:
-			self._current_chapter_json = self.scrapper.get(f"https://mangadex.org/api/chapter/{chap_id}").json()
-		except json.decoder.JSONDecodeError:
-			raise DownedSite("Mangadex")
+        self.npage = 1
+        # creating first image url
+        self.image = f"{self._server_url}/data/{self._current_chap['attributes']['hash']}/{self._current_chap['attributes']['data'][0]}"
 
-		self.chapter_name = self._current_chapter_json.get("title").replace("/", "-")
+    def is_last_chapter(self):
+        return self._chapinfo.index(self._current_chap)+1 == len(self._chapinfo)
 
-		self.chapter_number = str(self._current_chapter_json.get("chapter")).split(".")[0].zfill(3)
-		if "." in str(self._current_chapter_json.get("chapter")):
-			self.chapter_number += "." + str(self._current_chapter_json.get("chapter")).split(".")[1]
+    def is_last_image(self):
+        return self.npage == len(self._current_chap["attributes"]["data"])
 
-		self._img_root = f"{self._current_chapter_json.get('server')}{self._current_chapter_json.get('hash')}/"
-		self._img_ids = self._current_chapter_json.get("page_array")
+    def next_chapter(self):
+        next_index = self._chapinfo.index(self._current_chap) + 1
+        self.go_to_chapter(self._chapinfo[next_index]["attributes"]["chapter"])
 
-		# checking if chapter is accessible
-		if not self._img_ids:
-			if self._current_chapter_json.get("status") == "delayed":
-				ts = self._current_chapter_json.get("timestamp")
-				date = datetime.fromtimestamp(ts)
-				raise DelayedRelease(f"{self.manga_name}, chap {self.chapter_number}", date.isoformat(sep=" "))
+    def next_image(self):
+        self.image = f"{self._server_url}/data/{self._current_chap['attributes']['hash']}/{self._current_chap['attributes']['data'][self.npage]}"
+        self.npage += 1
 
-			raise EmptyChapter(self.manga_name, self.chapter_number)
+    @classmethod
+    def scan(cls, link:str=None, manga:str=None, lang=""):
+        # language stuff to fetch for specific locales
+        if lang:
+            cls._lang = lang
 
-		self.image = f"{self._img_root}{self._img_ids[0]}"
-		self.ext = self._img_ids[0].split(".")[1]
+        # init some api stuff that will be useful later
+        api_base = "https://api.mangadex.org"
+        api_manga_feed = api_base + "/manga/{uuid}/feed?limit=500&locales[]={locale}&order[chapter]=asc&offset={offset}"
 
-	def next_image(self):
-		"""
-		Goes to the next image in the scan being fetched.
-		"""
+        # getting the legacy id from the links
+        # TODO: change that when the new links are out while giving a tool to do the change
+        if manga is not None:
+            legacy_id = int(manga)
+        elif link is not None:
+            if link[-1] == "/":
+                link = link[:-1]
+            legacy_id = int(link.split("/")[-2])
 
-		self.image = f"{self._img_root}{self._img_ids[self.npage]}"
-		self.ext = self._img_ids[self.npage].split(".")[-1]
-		self.npage += 1
+        # getting the new id from the legacy ones
+        req = requests.post(api_base + "/legacy/mapping", json={"type": "manga", "ids": [legacy_id]})
+        if req.status_code != 200:
+            raise MangaNotFound(f" legacy id {legacy_id}")
+        id = req.json()[0]["data"]["attributes"]["newId"]
 
-	def is_last_image(self):
-		"""
-		Checks if it's the last image in the current chapter.
-		:rtype: bool
-		"""
+        # getting the chapter info from the feed
+        feed_req = requests.get(api_manga_feed.format(uuid=id, locale=cls._lang, offset=0))
+        feed = feed_req.json()
+        chapinfo = [float(chap["data"]["attributes"]["chapter"]) if "." in chap["data"]["attributes"]["chapter"] else int(chap["data"]["attributes"]["chapter"]) for chap in feed["results"]]
+        for i in range(500, int(feed["total"]), 500):
+            feed_req = requests.get(api_manga_feed.format(uuid=id, locale=cls._lang, offset=i))
+            chapinfo.extend([float(chap["data"]["attributes"]["chapter"]) if "." in chap["data"]["attributes"]["chapter"] else int(chap["data"]["attributes"]["chapter"]) for chap in feed_req.json()["results"]])
 
-		return self.npage == len(self._img_ids)
-
-	def go_to_chapter(self, chap_num):
-		"""
-		Make the fetcher go to the asked chapter.
-
-		:param chap_num: chapter number that was asked for
-		:type chap_num: int/str/float
-
-		:raises MangaNotFound: the asked chapter doesn't exist
-		"""
-
-		self._chap_id_pos = 0
-		while self._chap_id_pos < len(self._ordered_chaps_json) and self._ordered_chaps_json[self._chap_id_pos][1].get("chapter") != str(chap_num):
-			self._chap_id_pos += 1
-		if self._chap_id_pos >= len(self._ordered_chaps_json):
-			raise MangaNotFound(f"{self.manga_name}, chapter {chap_num}")
-
-		self._set_current_chap_info(self._ordered_chaps_json[self._chap_id_pos][0])
-
-	def next_chapter(self):
-		"""
-		Goes to the next chapter
-		"""
-
-		self._chap_id_pos += 1
-		self._set_current_chap_info(self._ordered_chaps_json[self._chap_id_pos][0])
-
-	def is_last_chapter(self):
-		"""
-		Checks if the current chapter is the last available one
-
-		:rtype: bool
-		"""
-
-		return self._chap_id_pos+1 == len(self._ordered_chaps_json)
-
-	@classmethod
-	def scan(cls, link:str=None, manga:str=None, lang:str=""):
-
-		header = {
-			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36",
-			"Set-Cookie": "domain=.mangadex.org",
-			"Path": "/"
-		}
-
-		if lang:
-			cls._lang = lang
-
-		if link is not None:
-			if link[-1] == "/":
-				link = link[:-1]
-			manga_id = link.split("/")[-2]
-		elif manga.isdecimal():
-			manga_id = manga
-		else:
-			raise MangaNotFound(manga)
+        # checking if there are no duplicate numbers for chapters
+        unique_chaps = {chap for chap in chapinfo}
+        if len(chapinfo) != len(unique_chaps):
+            new_chap_list = []
+            for i in unique_chaps:
+                new_chap_list.append(next(chap for chap in chapinfo if chap==i))
+            chapinfo = sorted(new_chap_list)
 
 
-		req = requests.get(f"https://mangadex.org/api/manga/{manga_id}", headers=header)
-		if req.status_code == 200:
-			manga_json = req.json()
-		else:
-			raise DownedSite("Mangadex")
-
-		if manga_json.get("status") == "Manga ID does not exist.":
-			raise MangaNotFound(manga_id)
-
-		if manga_json.get("chapter"):
-			chaps = [chap.get("chapter") for chap in manga_json.get("chapter").values() if chap.get("lang_code") == cls._lang and chap.get("chapter") != ""]
-		else:
-			return []
-
-		return list({float(chap) if "." in chap else int(chap) for chap in chaps})
-
-	def quit(self):
-		"""
-		Method used to close everything that was used after finishing to use the fetcher
-		"""
-
-		self.scrapper.close()
+        return chapinfo
 
 
 class MangadexEN(Mangadex):
-	_lang = "gb"
+    _lang = "en"
 
 
 class MangadexFR(Mangadex):
-	_lang = "fr"
-
+    _lang = "fr"
